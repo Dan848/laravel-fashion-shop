c77a8f8003aafa49d3013ecddb8baea58ad94cbd
NelloRossi <rossianiello97@gmail.com>
2023-06-09 12:46:36 +0200

    added controller routes

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Brand;
+use Illuminate\Support\Str;
 
 class BrandSeeder extends Seeder
 {
@@ -14,6 +16,12 @@ class BrandSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $brands = config('db_fashion.brands');
+        foreach ($brands as $brand) {
+            $newBrand = new Brand;
+            $newBrand->name = $brand;
+            $newBrand->slug = Str::slug($newBrand->name, '-');
+            $newBrand->save();
+        }
     }
 }
