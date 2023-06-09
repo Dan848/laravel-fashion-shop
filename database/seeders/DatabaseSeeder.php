f98c0f2b3e1546b1cb1a83ceb2fcc806509819ec
NelloRossi <rossianiello97@gmail.com>
2023-06-09 15:36:43 +0200

    added function to seed all tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            BrandSeeder::class,
+            CategorySeeder::class,
+            TextureSeeder::class,
+            ColorSeeder::class,
+            ProductSeeder::class
+        ]);
     }
 }
