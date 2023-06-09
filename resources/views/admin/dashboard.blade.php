f0bba325321192096fc2bcd4e2180f25802236c0
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-06-09 15:46:35 +0200

    fix route on dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,24 +1,25 @@
 @extends('layouts.app')
 
-@section("title")
-Dashboard
+@section('title')
+    Dashboard
 @endsection
 
 @section('content')
-<div class="container-fluid px-4">
-    <h1 class="mt-4">Dashboard</h1>
-    <ol class="breadcrumb mb-4">
-        <li class="breadcrumb-item active">Dashboard</li>
-    </ol>
-    <div class="col-auto">
-        <div class="card mb-4">
-            <div class="card-header">
-                <i class="fas fa-chart-bar me-1"></i>
-                Dashboard
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Dashboard</h1>
+        <ol class="breadcrumb mb-4">
+            <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
+        <div class="col-auto">
+            <div class="card mb-4">
+                <div class="card-header">
+                    <i class="fas fa-chart-bar me-1"></i>
+                    Dashboard
+                </div>
+                <div class="card-body"><a class="text-secondary" href="{{ route('admin.products.index') }}">Vedi tutti i
+                        Progetti</a></div>
+                <div class="card-footer small text-muted">Updated</div>
             </div>
-            <div class="card-body"><a class="text-secondary" href="#">Vedi tutti i Progetti</a></div>
-            <div class="card-footer small text-muted">Updated</div>
         </div>
     </div>
-</div>
 @endsection
