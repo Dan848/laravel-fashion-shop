c681a4088cca88a0e2fec0cd9b5a43097122b8ad
Dan848 <daniele.ceravolo15@gmail.com>
2023-06-09 12:45:47 +0200

    add product migration and seeder

--- database/migrations/2023_06_09_100125_create_products_table.php
+++ database/migrations/2023_06_09_100125_create_products_table.php
@@ -15,6 +15,22 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('old_id')->unique();
+            $table->string('name', 200)->unique();
+            $table->string('slug')->unique();
+            $table->string('image_link')->nullable();
+            $table->text('description')->nullable();
+            $table->double('price',  10,2)->nullable();
+
+            $table->unsignedBigInteger('brand_id')->nullable();
+            $table->foreign('brand_id')->references("id")->on("brands")->onDelete("set null");
+
+            $table->unsignedBigInteger('category_id')->nullable();
+            $table->foreign('category_id')->references("id")->on("categories")->onDelete("set null");
+
+            $table->unsignedBigInteger('texture_id')->nullable();
+            $table->foreign('texture_id')->references("id")->on("textures")->onDelete("set null");
+;
             $table->timestamps();
         });
     }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use Illuminate\Support\Str;
 
 class ProductSeeder extends Seeder
 {
@@ -14,6 +16,20 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $products = config("db_fashion.products");
+        foreach($products as $product) {
+            $newProduct = new Product();
+            $newProduct->old_id = $product["old_id"];
+            $newProduct->name = $product["name"];
+            $newProduct->slug = Str::slug($newProduct->name, "-");
+            $newProduct->image_link = $product["image_link"];
+            $newProduct->description = $product["description"];
+            $newProduct->price = $product["price"];
+            $newProduct->brand_id = $product["brand_id"];
+            $newProduct->category_id = $product["category_id"];
+            $newProduct->texture_id = $product["texture_id"];
+            $newProduct->name = $product["name"];
+            $newProduct->save();
+        }
     }
 }
