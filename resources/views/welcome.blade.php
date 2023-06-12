58dc467cc00a4862b2d62140bc67f5499136bcd9
Dan848 <daniele.ceravolo15@gmail.com>
2023-06-12 12:20:04 +0200

    bigger logo fix title add favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <div class="d-flex align-items-center">
         <div class="display-1 fw-bolder d-inline align-middle m-0 p-0">
             Welcome to
-            <span class="text-bg-primary ps-3 rounded-start-3">Bug</span><span class="text-bg-secondary pe-3 rounded-end-3">Makeup</span>
+            <span class="text-bg-primary ps-3 rounded-start-3">Bug</span><span class="text-bg-secondary pe-3 rounded-end-3">MakeUp</span>
             {{-- <span class="text-primary">PRO</span><span class="text-secondary">JECT</span> --}}
 
         </div>
