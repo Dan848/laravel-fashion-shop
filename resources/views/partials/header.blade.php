d8e7aee36964ae4e71cf88812faacb814e335cce
G-Lumia <giorgiolumia@gmail.com>
2023-06-12 12:16:14 +0200

    header and footer fixed, home page resized

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,42 @@
+<header>
+    <nav class="h-100 sb-topnav navbar navbar-expand navbar-light bg-primary">
+                <!-- Navbar Brand-->
+                <a class="navbar-brand ms-3 p-2 fw-medium hover-scale" href="{{ url('/') }}">Bug Make-Up
+                    <span><img src="/images/logo/cute_bug.png" alt="logo" width="25px" height="25px"></span>
+                </a>
+                <!-- Navbar Search-->
+                <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+                    <div class="input-group">
+                        <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
+                        <button class="btn btn-secondary hover-invert" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+                    </div>
+                </form>
+                <!-- Navbar DropDown-->
+                <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                    <!-- Authentication Links -->
+                    @guest
+                    <li class="nav-item">
+                        <a class="nav-link active hover-invert" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link active hover-invert" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                    @endif
+                    @else
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">{{ Auth::user()->name }}<i class="fas fa-user fa-fw"></i></a>
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="{{ url('admin') }}">{{__('Dashboard')}}</a></li>
+                            <li><hr class="dropdown-divider" /></li>
+                            <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </ul>
+                    </li>
+                    @endguest
+                </ul>
+            </nav>
+</header>
